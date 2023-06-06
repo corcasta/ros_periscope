6172dd70d51dbbabf2bf97ab5c8be41aed4e8726
corcasta <corcasta@outlook.com>
2023-06-06 10:14:09 +0200

    Updating px4 listener

--- periscope/periscope/nodes/px4_sensor_listener.py
+++ periscope/periscope/nodes/px4_sensor_listener.py
@@ -3,6 +3,7 @@ import numpy as np
 import transforms3d as tf
 from rclpy.node import Node
 from px4_msgs.msg import SensorCombined, VehicleOdometry
+from geometry_msgs.msg import TransformStamped, PoseWithCovarianceStamped
 from rclpy.qos import qos_profile_sensor_data
 #import px4_ros_com.src.lib
 
@@ -17,8 +18,15 @@ class SensorCombinedListener(Node):
                                                      "/fmu/out/vehicle_odometry",#"/fmu/out/sensor_combined",
                                                      self.demo_callback,
                                                      qos_profile_sensor_data)
-          
-    
+
+
+        self.publisher = self.create_publisher(PoseWithCovarianceStamped, 
+                                               'drone_posecov',
+                                               10)
+        
+        self.pwcs = PoseWithCovarianceStamped()
+        
+        
     def __qned2enu(self, q_ned):
         # first a pi/2 rotation around the Z-axis (down)
         qz = tf.quaternions.axangle2quat([0,0,1], np.pi/2)
@@ -65,6 +73,20 @@ class SensorCombinedListener(Node):
         position_ned = tf.quaternions.rotate_vector(position_enu, q_ned)
         print("NED Position: ", position_ned)
         print("NED q: ", q_ned)
+        
+        
+        
+        self.pwcs.pose.pose.position.x = msg.position[0]
+        self.pwcs.pose.pose.position.y = msg.position[1]
+        self.pwcs.pose.pose.position.z = msg.position[2]
+        
+        
+        self.pwcs.pose.pose.orientation.x = msg.q[0]
+        self.pwcs.pose.pose.orientation.y = msg.q[1] 
+        self.pwcs.pose.pose.orientation.z = msg.q[2]
+        self.pwcs.pose.pose.orientation.w = msg.q[3]
+        
+        self.publisher.publish(self.pwcs)
         
         
    
